74e12ecd01dd5882e451408baa4657d496bf7359
root <root@08e202e0-8cfd-48b8-b4b6-8f3e32338303.cloudnine.internal>
2016-06-23 15:44:03 +0900

    modify

--- export_json.sh
+++ export_json.sh
@@ -4,4 +4,4 @@ if [ -e af_feedmod.json ]; then
   /usr/bin/mv /pub/ttrss/plugins.local/af_feedmod/af_feedmod.json /pub/ttrss/plugins.local/af_feedmod/af_feedmod.json.`/usr/bin/date +%Y%m%d%H%M%S`
 fi
 
-/usr/bin/su -s /bin/bash apache -c '/usr/bin/php /pub/ttrss/plugins.local/af_feedmod/export_json.php' > af_feedmod.json
+/usr/bin/su -s /bin/bash apache -c '/usr/bin/php /pub/ttrss/plugins.local/af_feedmod/export_json.php | /usr/bin/jq ''.''' > af_feedmod.json
