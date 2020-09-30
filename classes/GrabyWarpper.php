78d36b8141d01a10cbf8899b4c5e79aac24d3645
makoto_kozono <oceandepthz@gmail.com>
2020-09-30 11:46:43 +0900

    modify

--- classes/GrabyWarpper.php
+++ classes/GrabyWarpper.php
@@ -5,6 +5,10 @@ use Graby\Graby;
 
 class GrabyWarpper {
     function get_html($url) : string{
+        if(!filter_var($url, FILTER_VALIDATE_URL)){
+            return "";
+        }
+
         $graby = new Graby();
         $result = $graby->fetchContent($url);
         if($result['status'] === 200){
